7d2683202fc52fe7650d67c6c33ddc8d7a4967b5
hekate2 <howsoonisnow1121@gmail.com>
2021-02-13 21:07:57 -0800

    tryout

--- archive.js
+++ archive.js
@@ -4,12 +4,15 @@ $(document).ready(function() {
     
     jQuery.get('beige.txt', function(data) {
         src_array = data.split("\n");
-        alert(src_array);
+        for (var i = 0; i < 28; i++) {
+            alert(src_array[i]);
+        }
+        
     });
     
     function setSrc() {
         for (var i = 1; i < 28; i++) {
-            $('#' + i).attr('src', src_array[counter]);
+            $('#e' + i).attr('src', src_array[counter]);
             counter += 1;
         }
     }
