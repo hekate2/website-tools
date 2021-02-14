049e72f51dffab21472ec509f116fce8ab5af62b
hekate2 <howsoonisnow1121@gmail.com>
2021-02-13 23:13:06 -0800

    fixed text

--- archive.js
+++ archive.js
@@ -12,8 +12,7 @@ $(document).ready(function() {
     function setSrc() {
         for (var i = 1; i <= 28; i++) {
             $('#e' + i).attr('src', src_array[i]);
-            alert(src_array[i].substr(src_array[i].length - 12, src_array[i].length));
-            $('#e' + i).next().text(src_array[i].substr(src_array[i].length - 12, src_array[i].length));
+            $('#e' + i)parent().next().text(src_array[i].substr(src_array[i].length - 12, src_array[i].length));
         }
     }
     
