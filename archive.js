ee730621e89b453d003fbfa6f009158aad5b9a83
hekate2 <howsoonisnow1121@gmail.com>
2021-02-13 22:02:37 -0800

    tryout

--- archive.js
+++ archive.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     }
     
     function setSrc() {
-        for (var i = 1; i < 28; i++) {
+        for (var i = 1; i <= 28; i++) {
             $('#e1').attr('src', src_array[i]);
             $('#e' + i).attr('src', src_array[i]);
         }
