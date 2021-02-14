1e692cb19dffcff11693210fa968f72530fbf2cf
hekate2 <howsoonisnow1121@gmail.com>
2021-02-13 21:50:22 -0800

    tryout

--- archive.js
+++ archive.js
@@ -2,18 +2,22 @@ $(document).ready(function() {
     var src_array = [];
     var counter = 0;
     
-    jQuery.get('beige.txt', function(data) {
+    function readTxt() {
+        jQuery.get('beige.txt', function(data) {
         src_array = data.split("\n");
         setSrc();
-    });
+        });
+    }
     
     function setSrc() {
         for (var i = 1; i < 28; i++) {
             $('#e1').attr('src', src_array[i]);
             $('#e' + i).attr('src', src_array[i]);
-            alert('#e' + i);
         }
     }
-    setSrc();
+    
+    readTxt();
+    
+    // $('button').on('click', setSrc());
     
 }); 
